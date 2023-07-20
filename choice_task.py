8df4953328889420ee1ec0527512e342ed7e87ce
Eashan Sahai <esahai@sas.upenn.edu>
2023-07-20 00:21:41 -0600

    fixed another future bug with pump and trying to split the elifs

--- choice_task.py
+++ choice_task.py
@@ -21,10 +21,19 @@ class Choice:
 
     def squirrel_choice(self, ourChoice):
         while True:
-            if ((self.right_button.is_pressed() and ourChoice=='Right') or (self.left_button.is_pressed() and ourChoice=='Left')):
+            if (self.right_button.is_pressed() and ourChoice=='Right'):
                 print("Reward Given!")
+                pump = Pump()
                 pump.run_forward(50,5)
                 break
-            elif ((self.right_button.is_pressed() and ourChoice=='Right') or (self.left_button.is_pressed() and ourChoice=='Left')):
+            elif (self.left_button.is_pressed() and ourChoice=='Left'):
+                print("Reward Given!")
+                pump = Pump()
+                pump.run_forward(50,5)
+                break
+            elif (self.right_button.is_pressed() and ourChoice=='Right'):
+                print("Wrong Choice")
+                break
+            elif (self.left_button.is_pressed() and ourChoice=='Left'):
                 print("Wrong Choice")
                 break
